5a641b5cb6e8193ca873de969fdb3a7fb984fceb
Rafael Vidal <rafaelvieira12@hotmail.com>
2018-05-10 11:27:36 -0300

    [cobasi] - menu com links e estrutura de pastas

---
+++ js/scriptGeral.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+		  
+	// inicio pagina
+	$(".geral").load("pedidos/pedidos.html");
+
+	$("header nav a").click(function(e) {
+		e.preventDefault();
+
+		var nome = $(this).attr("href")
+		var textoNome = $(this).attr("data-text")
+
+		// coloca marcação no menu
+		$("header nav a").removeClass("marcado")
+		$(this).addClass("marcado")
+
+		// altera o texto do header
+		$("header h1 span").html(textoNome)
+
+		// mostra a página de acordo com menu
+		if($(this).attr('href')) {
+			$(".geral").load(nome + "/" + nome + ".html");
+		}
+			  
+	});
+
+});
